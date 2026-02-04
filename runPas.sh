107baecf6a3f9436e8d82905f8b8e64be3704abd
sm-vaal <sergiomv2006@gmail.com>
2026-02-04 13:23:50 +0100

    Added a cleanup mode with -c

--- runPas.sh
+++ runPas.sh
@@ -2,35 +2,43 @@
 
 if [ -z "$1" ]; then
     echo "ERROR: Invalid arguments"
-    echo "Usage:" "$0" " yourCode.pas [-k(eep)]"
+    echo "Usage:" "$0" " yourCode.pas [-k(eep) / -c(lean)]"
     exit 1
 fi
 
 filePath="$1"
 keepMode=0
+cleanMode=0
+
 if [ "$2" == "-k" ] || [ "$2" == "-K" ]; then
     keepMode=1
+fi
+
+if [ "$2" == "-c" ] || [ "$2" == "-C" ]; then
+    cleanMode=1
 fi
 
 tempObjectName="tmp.obj"
 tempListName="tmp.lst"
 tempOutName="tmp.out"
 
-if [ $keepMode -eq 1 ]; then
+if [ $keepMode -eq 1 ] || [ $cleanMode -eq 1 ]; then
     tempObjectName="$filePath"".obj"
     tempListName="$filePath"".lst"
     tempOutName="$filePath"".out"
 fi
 
-# run the compiler
-compiler="./pfccomp"
-echo "---COMPILING---"
-"$compiler" "$filePath" "$tempListName" "$tempObjectName"
+if [ $cleanMode -eq 0 ]; then
+	# run the compiler
+	compiler="./pfccomp"
+	echo "---COMPILING---"
+	"$compiler" "$filePath" "$tempListName" "$tempObjectName"
 
-# run the interpreter
-interpreter="./pint"
-echo "---RUNNING---"
-"$interpreter" "$tempObjectName" "$tempOutName"
+	# run the interpreter
+	interpreter="./pint"
+	echo "---RUNNING---"
+	"$interpreter" "$tempObjectName" "$tempOutName"
+fi
 
 # cleanup if keep mode is off
 if [ $keepMode -eq 0 ]; then
